ba399ffa4bc3cf55c4d934b3232f9d315a42471c
Jaime Montes de Oca <jaime0422@live.com>
2022-04-24 22:50:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Jaime is the champion";
+
+document.body.append(newHeader);
